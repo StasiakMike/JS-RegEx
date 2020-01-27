d7a003c449003a0e3eeb5dc13e669bbd5e13dc1e
Mike <mavr@poczta.onet.pl>
2020-01-27 22:41:58 +0100

    ready app

---
+++ validation.js
@@ -0,0 +1,22 @@
+const inputs = document.querySelectorAll('input');
+
+const patterns = {
+    username: /^[a-z\d]{5,12}$/i,
+    email: /^([a-x\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+    password: /^[\w@-]{8,20}$/i,
+    telephone:/^\d{11}$/
+};
+
+function validate(field,regex){
+    if(regex.test(field.value)){
+        field.className = 'valid';
+    } else {
+        field.className = 'invalid';
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+        validate(e.target, patterns[e.target.attributes.name.value])
+    });
+});
